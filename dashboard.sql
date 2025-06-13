49319f09670f754f42d6eb168cbb0d51b4ef17e2
magasalamanca <maga.29222@gmail.com>
2025-06-13 20:10:23 +0500

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -1,21 +1,30 @@
--- Пример запросов
-
 -- 1. Сводка по ROI
 SELECT
     utm_source,
     utm_campaign,
-    SUM(total_cost) AS cost,
-    SUM(revenue) AS revenue,
-    ROUND((SUM(revenue) - SUM(total_cost)) / SUM(total_cost) * 100, 2) AS roi
-FROM marketing_data
-GROUP BY utm_source, utm_campaign
-ORDER BY roi DESC;
+    SUM(total_cost) AS marketing_cost,
+    SUM(revenue) AS total_revenue,
+    ROUND(
+        (SUM(revenue) - SUM(total_cost)) / 
+        NULLIF(SUM(total_cost), 0) * 100, 
+        2
+    ) AS roi_percentage
+FROM 
+    marketing_data
+GROUP BY 
+    utm_source, 
+    utm_campaign
+ORDER BY 
+    roi_percentage DESC;
 
 -- 2. Расчёт CPL, CPPU
 SELECT
     utm_source,
     utm_campaign,
-    SUM(total_cost) / NULLIF(SUM(leads_count), 0) AS cpl,
-    SUM(total_cost) / NULLIF(SUM(purchases_count), 0) AS cppu
-FROM marketing_data
-GROUP BY utm_source, utm_campaign;
+    SUM(total_cost) / NULLIF(SUM(leads_count), 0) AS cost_per_lead,
+    SUM(total_cost) / NULLIF(SUM(purchases_count), 0) AS cost_per_purchase
+FROM 
+    marketing_data
+GROUP BY 
+    utm_source, 
+    utm_campaign;
